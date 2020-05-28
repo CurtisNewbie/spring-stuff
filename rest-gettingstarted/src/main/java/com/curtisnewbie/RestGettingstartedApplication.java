a638f2ad2d9739de8e32beae1f4bfc1e025aab5b
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-28 12:23:04 +0800

    rest getting started

---
+++ rest-gettingstarted/src/main/java/com/curtisnewbie/RestGettingstartedApplication.java
@@ -0,0 +1,12 @@
+package com.curtisnewbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGettingstartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestGettingstartedApplication.class, args);
+	}
+}
